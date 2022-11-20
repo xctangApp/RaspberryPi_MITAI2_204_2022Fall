fdde41a57470d03bd6ad674dcb6424cc25436f5a
xctangApp <99854995+xctangApp@users.noreply.github.com>
2022-11-19 21:09:07 -0600

    Create TestGPIO_LED.py

---
+++ Lecture03/TestGPIO_LED.py
@@ -0,0 +1,16 @@
+#This code is running in Mu 1.0.2
+#204 LED Control Program
+#Date: 2022-11-09
+#Owner: X.Tang
+#Version: 1.0
+
+from gpiozero import LED
+from time import sleep
+
+led = LED(2)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
